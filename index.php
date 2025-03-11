9ccd974acf047a833d14322cff003cf616fc8d8c
Rafael Yohanan Farina <ra2457013>
2025-03-11 19:20:57 -0300

    primeira

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php  
+    $cidade = "Bauru";
+    $dia = 6;
+    $ano = 2024;
+    $mes = "Marco";//definir variaveis
+    echo "<b>$cidade</b>,$dia de $mes de $ano";//imprimir a frase com as variaveis e em negrito
+?>
